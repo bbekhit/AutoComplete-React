5a9fae4f2b8ca087626fa1473a3649c7bbcdaf47
Boutros(Peter) Bekhit <41764681+bbekhit@users.noreply.github.com>
2018-12-06 23:21:49 -0800

    Update NoResults.js

--- client/src/components/layout/NoResults.js
+++ client/src/components/layout/NoResults.js
@@ -4,8 +4,8 @@ const NoResults = () => {
   return (
     <div className="container">
        <div className="row justify-content-center d-flex flex-column align-items-center" style={{marginTop:"200px"}}>
-         <i className="fa fa-frown-o fa-2x"/>
-         <div>Oops!! No Results Found</div>
+         <i className="fa fa-frown-o fa-4x"/>
+         <div style={{fontSize:"45px"}}>Oops!! No Results Found</div>
        </div>
     </div>
   )
